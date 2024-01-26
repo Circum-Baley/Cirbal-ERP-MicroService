f0966ee068c84514b24cfdbfc6ada95fff4bfab3
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-01-26 02:11:04 -0300

    Se añade el microservicio COMMONS compartidas por varios
    microservicios en el módulo 'springboot-service-commons'

---
+++ springboot-service-commons/src/test/java/com/cirbal/springboot/app/commons/SpringbootServiceCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cirbal.springboot.app.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
