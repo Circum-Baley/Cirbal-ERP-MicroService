85fa5340cb912e1bde3b44cee8e57d35eb3414a3
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-01-28 03:58:16 -0300

    Configuración de la aplicación como cliente Eureka.
    Se configura la aplicación como cliente Eureka para el registro en el
    servidor de descubrimiento, tamboen se ha habilitado Feign para la
    comunicación con otros microservicios.
    Se mejora la organización del código.

--- springboot-service-item/src/main/java/com/cirbal/springboot/app/item/SpringbootServiceItemApplication.java
+++ springboot-service-item/src/main/java/com/cirbal/springboot/app/item/SpringbootServiceItemApplication.java
@@ -3,15 +3,14 @@ package com.cirbal.springboot.app.item;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 //@ComponentScan("com.cirbal.springboot.app.item")
-@EnableHystrix
-@EnableEurekaClient
+//@ComponentScan("com.cirbal.springboot.app.item.clientes")
 @EnableFeignClients
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringbootServiceItemApplication {
 
 	public static void main(String[] args) {
