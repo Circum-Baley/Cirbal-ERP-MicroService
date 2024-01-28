678d96aa0133d1fff0b1207f071f86b872da662e
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-01-28 03:54:19 -0300

    Controlador de Item con Resilience4J y ResponseEntity
    Se ha mejorado los Endpoints de Items para utilizar ResponseEntity en
    las respuestas, por otro lado se ha integrado Resilience4J para
    implementar circuit breaking en las llamadas a servicios externos.
    Se hace manejo de errores y fallback methods para asegurar la
    resiliencia y disponibilidad del sistema.

--- springboot-service-item/src/main/java/com/cirbal/springboot/app/item/controllers/ItemController.java
+++ springboot-service-item/src/main/java/com/cirbal/springboot/app/item/controllers/ItemController.java
@@ -1,57 +1,116 @@
+
 package com.cirbal.springboot.app.item.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Env;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cirbal.springboot.app.item.models.Item;
-import com.cirbal.springboot.app.item.models.Product;
+import com.cirbal.springboot.app.commons.models.entity.Product;
 import com.cirbal.springboot.app.item.services.ItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-//@EnableHystrix
+
 @RestController
+//@RequestMapping("/api/items")
 public class ItemController {
 
+	private static final Logger log = LoggerFactory.getLogger(ItemController.class);
+
 	@Autowired
-	@Qualifier("itemServiceImpl")
+	private CircuitBreakerFactory<?, ?> circutBFactory;
+	@Autowired
+	@Qualifier("itemServiceFeign")
 	private ItemService itemService;
 
 	@GetMapping("/list")
-	public List<Item> findAll(@RequestParam) {
-		return itemService.findAll();
+	public List<Item> findAlll() {
+		List<Item> item = itemService.findAll();
+		return item;
 	}
 
-	
-	@HystrixCommand(fallbackMethod = "metodoAlternativo")
+// The code above, is the same up, but this code, is with ResponseEntity
+	@GetMapping("/listarr")
+	public ResponseEntity<List<Item>> findAll() {
+		List<Item> items = itemService.findAll();
+		try {
+			if (items.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<>(items, HttpStatus.OK);
+		} catch (Exception e) {
+			log.error("Error while fetching items: {}", e.getMessage());
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+//	@HystrixCommand(fallbackMethod = "fallbackForFindById")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
-	public Item findById(@PathVariable Long id, @PathVariable Integer cantidad) {
-		return itemService.findById(id, cantidad);
+	public ResponseEntity<Item> findById(@PathVariable Long id, @PathVariable Integer cantidad) {
+		Item item = itemService.findByIdCantidad(id, cantidad);
+		return new ResponseEntity<>(item, HttpStatus.OK);
 	}
 
-	public Item metodoAlternativo(Long id, Integer cantidad) {
+	// Fallback method for findById
+	public ResponseEntity<Item> fallbackForFindById(Long id, Integer cantidad) {
+		Item item = new Item();
+		return new ResponseEntity<>(item, HttpStatus.OK);
+	}
+
+	@GetMapping("/detailProduct/{id}")
+	public Product findById(@PathVariable Long id) {
+		Product itemByProductId = itemService.findById(id);
+		return itemByProductId;
+	}
+
+//	@HystrixCommand(fallbackMethod = "metodoAlternativo")
+	@GetMapping("/verr/{id}/cantidad/{cantidad}")
+	public Item findByIdd(@PathVariable Long id, @PathVariable Integer cantidad) {
+//		return itemService.findById(id, cantidad);
+		return circutBFactory.create("items").run(() -> itemService.findByIdCantidad(id, cantidad),
+				e -> metodoAlternativo(id, cantidad, e));
+	}
+
+	public Item metodoAlternativo(Long id, Integer cantidad, Throwable e) {
+		log.info(e.getMessage());
 		Item item = new Item();
 		Product product = new Product();
 		item.setCantidad(cantidad);
 		product.setId(id);
-		product.setName(" Producto Por Defecto Pero En Si Se Puede Programar BCE");
-		product.setPrice(000.0);
-		product.setCreatedAt(null);
-		product.setPort(1111);
+		product.setName("Producto No Localizado ");
+		product.setPrice(000.00);
 		item.setProduct(product);
 		return item;
 	}
 
-	@DeleteMapping("/delete/{id}")
-	public void deleteById(@PathVariable Long id) {
-		itemService.deleteById(id);
+	@PostMapping("/createProduct")
+	@ResponseStatus(code = HttpStatus.OK)
+	public Product create(@RequestBody Product product) {
+		Product productCreated = itemService.create(product);
+		return productCreated;
 	}
 
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+		itemService.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	@PutMapping("/updateProduct/{id}")
+	public Product updateProduct(@RequestBody Product product, @PathVariable Long id) {
+		Product ItemByIdProduct = itemService.updateProduct(product, id);
+		return ItemByIdProduct;
+	}
 }
