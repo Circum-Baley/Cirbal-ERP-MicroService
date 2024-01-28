9e01cdff637366a47df45bb144b769ad6c6f9bca
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-01-28 03:43:46 -0300

    Integración Cliente Feign para Comunicación con Microservicio de PRODUCT
    A la vez, se ha implementado un cliente Feign denominado
    ProductoClienteRest para facilitar la comunicación con el microservicio
    (service-products). Este cliente proporciona  operaciones como listar
    productos, obtener detalles por ID, eliminar, crear y actualizar. Este
    cliente optimiza la interacción entre los microservicios, mejorando la
    eficiencia y mantenibilidad del sistema.

--- springboot-service-item/src/main/java/com/cirbal/springboot/app/item/clientes/ProductoClienteRest.java
+++ springboot-service-item/src/main/java/com/cirbal/springboot/app/item/clientes/ProductoClienteRest.java
@@ -6,20 +6,29 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import com.cirbal.springboot.app.item.models.Product;
+import com.cirbal.springboot.app.commons.models.entity.Product;
+
 
 @FeignClient(name = "service-products")
 public interface ProductoClienteRest {
-	
-	
+
 	@GetMapping("/list")
 	public List<Product> listar();
 
-	@GetMapping("/ver/{id}")
+	@GetMapping("/detail/{id}")
 	public Product detaildById(@PathVariable Long id);
-	
+
 	@DeleteMapping("/delete/{id}")
 	public void deleteById(@PathVariable Long id);
 
+	@PostMapping("/createProduct")
+	public Product create(@RequestBody Product product);
+
+	@PutMapping("/update/{id}")
+	public Product updateProduct(@RequestBody Product product, @PathVariable Long id);
 }
