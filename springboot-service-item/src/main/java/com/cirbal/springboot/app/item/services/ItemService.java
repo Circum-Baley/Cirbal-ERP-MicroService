81b19164c061ea1f5d63d337bd95cc7b4e6a1fd2
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-01-29 23:16:34 -0300

    Interfaz ItemService para realizar operaciones en carrito de compra.
    Se agrega interfaz ItemService con métodos
    para operaciones CRUD en elementos del carrito.

--- springboot-service-item/src/main/java/com/cirbal/springboot/app/item/services/ItemService.java
+++ springboot-service-item/src/main/java/com/cirbal/springboot/app/item/services/ItemService.java
@@ -2,11 +2,21 @@ package com.cirbal.springboot.app.item.services;
 
 import java.util.List;
 
+import com.cirbal.springboot.app.commons.models.entity.Product;
 import com.cirbal.springboot.app.item.models.Item;
 
 public interface ItemService {
 
 	public List<Item> findAll();
-	public Item findById(Long id,Integer cantidad);
-	public void deleteById(Long id);	
+
+	public Product findById(Long id);
+
+	public Item findByIdCantidad(Long id, Integer cantidad);
+
+	public void deleteById(Long id);
+
+	public Product create(Product product);
+
+	public Product updateProduct(Product product, Long id);
+
 }
