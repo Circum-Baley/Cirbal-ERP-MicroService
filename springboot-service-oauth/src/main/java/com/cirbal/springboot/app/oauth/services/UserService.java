8ff0ea821adab6fb109cac9c07181a0a7892788c
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-04-01 23:45:49 -0300

    Implementa el servicio de detalles de usuario en OAuth.
    Se ha creado la clase `UserService` en el paquete
    `com.cirbal.springboot.app.oauth.services` para implementar el servicio
    de detalles de usuario en el contexto de OAuth. Esta clase implementa la
    interfaz `UserDetailsService` de Spring Security y utiliza el cliente
    Feign `UserFeignClient` para recuperar los detalles del usuario desde el
    servicio de usuarios. Se manejan las excepciones si el usuario no existe
    y se construye un objeto `UserDetails` utilizando los datos recuperados
    del usuario.

---
+++ springboot-service-oauth/src/main/java/com/cirbal/springboot/app/oauth/services/UserService.java
@@ -0,0 +1,39 @@
+package com.cirbal.springboot.app.oauth.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cirbal.springboot.app.commons.users.models.entity.User;
+import com.cirbal.springboot.app.oauth.clients.UserFeignClient;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	@Autowired
+	private UserFeignClient userFeignClient;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findUserByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("Error, User don't exists;");
+		}
+		List<GrantedAuthority> authorities = user.getRoles().stream()
+				.map(role -> new SimpleGrantedAuthority(role.getName()))
+				.peek(authority -> logger.info("Role :" + authority.getAuthority())).collect(Collectors.toList());
+		logger.info("User" + username + "Authenticated");
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				user.getEnable(), true, true, true, authorities);
+	}
+}
