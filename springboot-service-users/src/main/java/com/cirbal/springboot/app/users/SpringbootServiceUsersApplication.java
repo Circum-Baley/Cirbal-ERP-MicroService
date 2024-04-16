0bdaaea1361b86a705a524f15b40169c3aa308ae
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-04-16 15:18:26 -0400

    Habilita Notacion @ComponentScan

---
+++ springboot-service-users/src/main/java/com/cirbal/springboot/app/users/SpringbootServiceUsersApplication.java
@@ -0,0 +1,19 @@
+package com.cirbal.springboot.app.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EntityScan({ "com.cirbal.springboot.app.commons.users.models.entity" })
+@ComponentScan("com.cirbal.springboot.app")
+public class SpringbootServiceUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServiceUsersApplication.class, args);
+	}
+
+}
