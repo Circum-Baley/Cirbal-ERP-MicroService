f1137d45a65493e1d947bb7018ae4baaee888de7
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-04-28 02:57:42 -0400

    Modifica dependencia en el pom.xml

---
+++ springboot-service-commons-user/src/test/java/com/cirbal/springboot/app/commons/users/SpringbootServiceCommonsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cirbal.springboot.app.commons.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceCommonsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
