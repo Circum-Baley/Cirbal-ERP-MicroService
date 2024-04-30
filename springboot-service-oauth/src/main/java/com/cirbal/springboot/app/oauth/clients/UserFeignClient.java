162789b627f957cf79ef92fb7a1f0ab98ee9d773
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-04-30 01:32:29 -0400

    Modifica Anotacion mal ubicadas

--- springboot-service-oauth/src/main/java/com/cirbal/springboot/app/oauth/clients/UserFeignClient.java
+++ springboot-service-oauth/src/main/java/com/cirbal/springboot/app/oauth/clients/UserFeignClient.java
@@ -3,24 +3,25 @@ package com.cirbal.springboot.app.oauth.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.cirbal.springboot.app.commons.users.models.entity.User;
 
-@FeignClient("service-users")
+@FeignClient(name = "service-users")
 public interface UserFeignClient {
 
 	@GetMapping("/users/search/find-username")
 	public User findByUsername(@RequestParam String username);
 
 	@PostMapping("/createUser")
-	public User createUser(User user);
+	public User createUser(@RequestBody User user);
 
 	@PutMapping("/updateUser/{id}")
-	public User updateUser(@RequestParam Long id, User us);
+	public User updateUser(@PathVariable Long id, @RequestBody User user);
 
 	@DeleteMapping("/userDelete/{id}")
-	public void userDelete(@RequestParam Long id);
+	public void userDelete(@PathVariable Long id);
 }
