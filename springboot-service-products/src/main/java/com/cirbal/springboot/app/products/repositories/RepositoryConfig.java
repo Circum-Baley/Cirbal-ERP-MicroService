d4a7511c6670db0af6dad6094f56e4cd46d5cad7
Circum-Baley <gonzalo.bahamondez@gmail.com>
2024-05-02 23:45:20 -0400

    Agrega metodo de configuracion para realizar la exposicipon de ID's en los request que se requiera.

---
+++ springboot-service-products/src/main/java/com/cirbal/springboot/app/products/repositories/RepositoryConfig.java
@@ -0,0 +1,19 @@
+package com.cirbal.springboot.app.products.repositories;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import com.cirbal.springboot.app.products.models.Product;
+
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+		config.exposeIdsFor(Product.class);
+	}
+
+}
